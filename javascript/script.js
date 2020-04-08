2aa7d74755cb96480378abd900b0555d21ebfce8
MehdiChaouche <mehdi.chaouche@le-campus-numerique.fr>
2020-04-08 14:57:59 +0200

    Exercice fait. Carousel fonctionnel.

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,37 @@
+const url = 'https://jsonplaceholder.typicode.com/users/1/posts';
 
-function fillTheCarousel(jsondata)
-{
+function fillTheCarousel(jsondata) {
+    jsondata.forEach(element => {
+        let carouseldiv = document.querySelector('#carousel');
+        let elementdiv = document.createElement('div');
+        let title = document.createElement('p');
+        title.textContent = element.title;
+        carouseldiv.appendChild(elementdiv);
+        elementdiv.appendChild(title);
+    })
 }
 
-$(document).ready(function()
-{
-    // Cliquer sur le bouton "Charger la liste" doit :
-    //  1. Gérer le click sur le bouton
-    //  2. Appeler une api (https://jsonplaceholder.typicode.com/users/1/posts)
-    //  3. Pour chaque élément de la liste, ajouter un élément au futur carousel
-    
-    //  4. Déclencher le carousel
-    $('#carousel').carouFredSel({
-        responsive: true,
-        align: "center",
-        direction: 'left'
+function apiCall() {
+    fetch(url) // asynchrone
+        .then(response => response.json())
+        .then(json => {
+// 3. Pour chaque élément de la liste, ajouter un élément au futur carousel
+            fillTheCarousel(json);
+// 4. Déclencer le carousel !
+            $('#carousel').carouFredSel({
+                responsive: true,
+                align: "center",
+                direction: 'left'
+            });
+        });
+}
+
+$(document).ready(function () {
+// Cliquer sur le bouton "Charger la liste" doit :
+// 1. Gérer le click sur le bouton
+    let bouton = document.querySelector('#declencheur');
+    bouton.addEventListener('click', function (event) {
+// 2. Appeler une api
+        apiCall();
     });
 });
